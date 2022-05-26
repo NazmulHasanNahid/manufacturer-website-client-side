d94a9cd1aa152f259efa74c292957c43b865e9e0
NazmulHasanNahid <nh.nahid4491@gmail.com>
2022-05-26 08:54:08 +0600

    cancel order implement

--- src/components/Dashboard/Myorder.js
+++ src/components/Dashboard/Myorder.js
@@ -3,6 +3,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../Auth/firebase.init';
 import { signOut } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 
 const Myorder = () => {
@@ -27,7 +28,25 @@ const Myorder = () => {
             })
             .then(data => setOrder(data))
 
-     },[user])
+     },[user , navigate])
+     
+     const handleDelet = (id) => {
+      const proceed = window.confirm("Are you sure you want to Cencel");
+      if (proceed) {
+        const url = `http://localhost:5000/order/${id}`;
+        fetch(url, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              const remaining = order.filter((product) => product._id !== id);
+              setOrder(remaining);
+              toast("Your Order Canceled");
+            }
+          });
+      }
+    };
      return (
           <div>
                <h2>my orders {order.length}</h2>
@@ -51,7 +70,7 @@ const Myorder = () => {
                <th>{index + 1}</th>
                <td>{o?.item?.name}</td>
                <td><img width={40} src={o.item.image} alt="" /></td>
-               <td><button className='btn btn-error'>Cancel</button></td>
+               <td><button onClick={()=>handleDelet(o._id)} className='btn btn-error'>Cancel</button></td>
                <td> <Link to={`/dashboard/payment/${o._id}`}><button className="btn btn-primary">Payment</button></Link> </td>
              </tr> )
       }
